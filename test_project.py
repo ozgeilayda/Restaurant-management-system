963580ff3e1b2031c9414cf8e6528a217a9dcb24
ozgeilayda <ozgeilayda.albayrak@std.yeditepe.edu.tr>
2025-12-21 18:45:53 +0300

    Update tests for calculate_bill and add split_bill tests
    Refactor test for calculate_bill and add new tests for split_bill.

--- test_project.py
+++ test_project.py
@@ -2,14 +2,13 @@
 
 from orders import calculate_bill
 
-def test_calculate_bill_simple():
-    order = {"items": [{"price": 10, "qty": 2}, {"price": 5, "qty": 1}] }
+def test_calculate_bill(): #hesaplama fonksiyonu doğru çalışıyor mu kontrolü için
+    order = {"items": [{"price": 10, "qty": 2}]}
     bill = calculate_bill(order, 0.1, 0.1)
-    
-    assert bill["subtotal"] == 25
-    
-    assert bill["tax"] == 2.5 
-    
-    assert bill["tip"] == 2.5 
-    
-    assert bill["total"] == 30
+    assert bill["total"] == 24
+
+def test_split_bill(): #faturayı eşit bölüp bölmediğini kontrol etmek için
+    assert split_bill(100,4) == 25
+
+def test_split_bill_invalid(): #geçersiz bir kişi sayısı girildiğinde hata vermeden None döndürmesini kontrol etmek için
+    assert split_bill(100,0) is None 
